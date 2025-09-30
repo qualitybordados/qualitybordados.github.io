63235d76806d53ea6d68e8328e65a473e6cdf12e
qualitybordados <quallitybordados@gmail.com>
2025-09-30 13:04:38 -0600

    1

---
+++ docs/firebase-config.js
@@ -0,0 +1,8 @@
+window.__QB_FIREBASE_CONFIG__ = {
+  apiKey: 'AIzaSyCteKUvTp4nz_F9YusNt2MTS8xr9qBoHSA',
+  authDomain: 'quality-bordados.firebaseapp.com',
+  projectId: 'quality-bordados',
+  storageBucket: 'quality-bordados.firebasestorage.app',
+  messagingSenderId: '901273614683',
+  appId: '1:901273614683:web:91a07c642f720c3c89027e',
+}
